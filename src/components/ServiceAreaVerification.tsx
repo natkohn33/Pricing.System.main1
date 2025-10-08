172810a344ac92734698bcdec5fe0dbdbbc3f7dd
natkohn33 <natkohn33@gmail.com>
2025-10-08 12:21:05 -0500

    Updated ServiceAreaVerification.tsx

--- src/components/ServiceAreaVerification.tsx
+++ src/components/ServiceAreaVerification.tsx
@@ -825,15 +825,14 @@ export function ServiceAreaVerification({
                                   </div>
                                 </div>
                               </div>
-              
-<div>
+<
 <div className="mt-2 pt-2 border-t border-yellow-200 opacity-0 group-hover/item:opacity-100 transition-opacity">
+  {/* Hover indicator */}
   <p className="text-xs text-yellow-600 flex items-center">
     <span className="mr-1">👆</span>
     Click to scroll to this location in the results below
   </p>
 
-  
                       {/* Footer with action guidance */}
                       <div className="mt-4 pt-3 border-t border-yellow-200">
                         <p className="text-xs text-yellow-700 text-center">
