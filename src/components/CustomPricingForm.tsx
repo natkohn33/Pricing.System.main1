ededaaac26e09d8445e25a1435d471c91e1abaf2
natkohn33 <natkohn33@gmail.com>
2025-10-08 17:01:25 -0500

    Updated CustomPricingForm.tsx

--- src/components/CustomPricingForm.tsx
+++ src/components/CustomPricingForm.tsx
@@ -669,7 +669,7 @@ export function CustomPricingForm({
       {/* Bulk Upload Workflow */},
       {!isSingleLocation && (
         <div className="space-y-6">
-      );
+   
           
           {/* Global Pricing Configuration */}
           <div className="bg-white border border-gray-200 rounded-lg p-6">
