04d7b3ab8b56f4e058b3e6fed7f4c7f480015773
natkohn33 <natkohn33@gmail.com>
2025-10-08 13:03:56 -0500

    Updated ServiceAreaVerification.tsx

--- src/components/ServiceAreaVerification.tsx
+++ src/components/ServiceAreaVerification.tsx
@@ -833,4 +833,3 @@ export function ServiceAreaVerification({
     <div className="absolute top-full left-8 w-0 h-0 border-l-8 border-r-8 border-t-8 border-l-transparent border-r-transparent border-t-yellow-300" />
   />
 );
-    
