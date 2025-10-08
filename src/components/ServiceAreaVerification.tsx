e7e654e5d300c80bb9e41e1fc51ddcbe01165c31
natkohn33 <natkohn33@gmail.com>
2025-10-08 13:03:07 -0500

    Updated ServiceAreaVerification.tsx

--- src/components/ServiceAreaVerification.tsx
+++ src/components/ServiceAreaVerification.tsx
@@ -829,7 +829,7 @@ export function ServiceAreaVerification({
     <span className="font-medium">💡 Tip:</span> Click on any location above to jump to its details in the verification results
   </p>
 </div>
-{/* Tooltip arrow */}
+ {/* Tooltip arrow */}
     <div className="absolute top-full left-8 w-0 h-0 border-l-8 border-r-8 border-t-8 border-l-transparent border-r-transparent border-t-yellow-300" />
-  </div>
+  </>
 )}
